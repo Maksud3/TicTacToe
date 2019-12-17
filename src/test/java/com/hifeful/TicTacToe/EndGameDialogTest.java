78e49fb7849a0afaf87e1bc33e6d8c7bb9abc570
Maksud3 <maksum.harris@yandex.ru>
2019-12-17 18:07:03 +0200

    I don't bevieve in that shit

---
+++ src/test/java/com/hifeful/TicTacToe/EndGameDialogTest.java
@@ -0,0 +1,7 @@
+package com.hifeful.TicTacToe;
+
+import static org.junit.Assert.*;
+
+public class EndGameDialogTest {
+
+}
---
+++ src/test/java/com/hifeful/TicTacToe/GamePanelTest.java
@@ -0,0 +1,16 @@
+package com.hifeful.TicTacToe;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GamePanelTest {
+
+    @Test
+    public void paintComponent() {
+    }
+
+    @Test
+    public void newGame() {
+    }
+}
